7bfa76b974a375e460697839694c40b31618f20f
Yannick <yannick@pc-131-86.customer.ask4.lan>
2022-02-27 23:15:43 +0000

    Added method 4 and 5 to energy_analysis.py

--- energy_analysis.py
+++ energy_analysis.py
@@ -155,9 +155,100 @@ class EnergyAnalysis:
         else:
             raise ValueError("Country does not exist.")
 
-     # method 6 -->
+    # method 4 -->
+    def consumption_country(self, countries):
+        """
+        Select the Countries, sum up the total per year and plot it
+
+        Parameters
+        ------------
+        countries: list
+            A list with all countries to be analyzed
+
+        Returns
+        ---------
+        Plot with consumption and countries
 
 
+        Example
+        ---------
+        object.consumption_country(["Switzerland", "Portugal", "Chile"])
+        """
+        
+        #Import the data
+        object.download_file()
+
+        #Create a list with all _consumption columns and create a new dataframe
+        consumption_list = self.df.filter(like='_consumption').columns
+        consumption_data = self.df[["country", "year",'biofuel_consumption','coal_consumption','fossil_fuel_consumption',
+        'gas_consumption', 'hydro_consumption', 'low_carbon_consumption',
+        'nuclear_consumption', 'oil_consumption', 'other_renewable_consumption',
+        'primary_energy_consumption', 'renewables_consumption',
+        'solar_consumption', 'wind_consumption']]
+        
+        #calculate the sum of all consumption per year
+        consumption_data["total"] = consumption_data[consumption_list].sum(axis=1)
+        
+        #Create a dataframe for every country needed and drop NaN
+        for i in countries:
+            globals()[i] = consumption[consumption["country"] == i]
+            indexNames = globals()[i][globals()[i]['total'] < 1 ].index
+            globals()[i].drop(indexNames , inplace=True)
+            
+        #plot the total consumption
+        for i in countries:
+            plt.plot(globals()[i]["year"], globals()[i]["total"], label=i)
+        plt.title('Consumption per Year', fontsize=14)
+        plt.xlabel('Year', fontsize=14)
+        plt.ylabel('Total Consumption', fontsize=14)
+        plt.grid(True)
+        plt.legend()
+        plt.show()
+
+    
+    # method 5 -->
+    def gdp_country(self,countries):
+        """
+        Select the Countries, and plot the gdp over the years
+
+        Parameters
+        ------------
+        countries: list
+            A list with all countries to be analyzed
+
+        Returns
+        ---------
+        Plot with gdp and countries
+
+
+        Example
+        ---------
+        object.consumption_country(["Switzerland", "Portugal", "Chile"])
+        """
+
+        #Import the data
+        object.download_file()
+        
+        #Select the columns Country, Year and gdp and create a new dataframe
+        gdp_data = self.df[["country","year","gdp"]]
+        
+        
+        #Create a dataframe for every country needed and drop NaN
+        for i in countries:
+            globals()[i] = gdp_data[gdp_data["country"] == i]
+            gdp_data.dropna(subset = ["gdp"], inplace=True)
+            
+        #plot the total consumption
+        for i in countries:
+            plt.plot(globals()[i]["year"], globals()[i]["gdp"], label=i)
+        plt.title('GDP per Year', fontsize=14)
+        plt.xlabel('Year', fontsize=14)
+        plt.ylabel('GDP per Year', fontsize=14)
+        plt.grid(True)
+        plt.legend()
+        plt.show()
+    
+    # method 6 -->
     def gapminder(self,y):
         
         """
